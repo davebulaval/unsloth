4ef866491c7282baaafa7ddb4b31f6c0c6afc2ca
davebulaval <david.beauchemin@ift.ulaval.ca>
2025-03-13 21:20:51 -0400

    black

--- unsloth/models/mistral.py
+++ unsloth/models/mistral.py
@@ -25,6 +25,7 @@ from transformers.models.mistral.modeling_mistral import (
     MistralModel,
     MistralForCausalLM,
 )
+
 # For Pytorch 2.1.1
 try:
     from transformers.models.mistral.modeling_mistral import (
@@ -32,7 +33,7 @@ try:
         MistralFlashAttention2,
     )
 except:
-    MistralSdpaAttention   = MistralAttention
+    MistralSdpaAttention = MistralAttention
     MistralFlashAttention2 = MistralAttention
 pass
 from unsloth_zoo.utils import Version, _get_dtype
@@ -40,18 +41,19 @@ from unsloth_zoo.utils import Version, _get_dtype
 
 def MistralAttention_fast_forward(
     self,
-    hidden_states:       torch.Tensor,
-    causal_mask:         Optional[BlockDiagonalCausalMask] = None,
-    attention_mask:      Optional[torch.Tensor] = None,
-    position_ids:        Optional[torch.LongTensor] = None,
-    past_key_value:      Optional[Tuple[torch.Tensor]] = None,
-    output_attentions:   bool = False,
-    use_cache:           bool = False,
-    padding_mask:        Optional[torch.LongTensor] = None,
+    hidden_states: torch.Tensor,
+    causal_mask: Optional[BlockDiagonalCausalMask] = None,
+    attention_mask: Optional[torch.Tensor] = None,
+    position_ids: Optional[torch.LongTensor] = None,
+    past_key_value: Optional[Tuple[torch.Tensor]] = None,
+    output_attentions: bool = False,
+    use_cache: bool = False,
+    padding_mask: Optional[torch.LongTensor] = None,
     position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
-    *args, **kwargs,
+    *args,
+    **kwargs,
 ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
-    
+
     # Clear inference
     if hasattr(self, "paged_attention"):
         del self.paged_attention_K
@@ -65,14 +67,14 @@ def MistralAttention_fast_forward(
 
     bsz, q_len, _ = hidden_states.size()
 
-    n_heads    = self.config.num_attention_heads
-    n_groups   = self.num_key_value_groups
+    n_heads = self.config.num_attention_heads
+    n_groups = self.num_key_value_groups
     n_kv_heads = self.config.num_key_value_heads
-    head_dim   = self.head_dim
-    assert(n_kv_heads * n_groups == n_heads)
+    head_dim = self.head_dim
+    assert n_kv_heads * n_groups == n_heads
 
     Q, K, V = self.apply_qkv(self, hidden_states)
-    Q = Q.view(bsz, q_len, n_heads,    head_dim).transpose(1, 2)
+    Q = Q.view(bsz, q_len, n_heads, head_dim).transpose(1, 2)
     K = K.view(bsz, q_len, n_kv_heads, head_dim).transpose(1, 2)
     V = V.view(bsz, q_len, n_kv_heads, head_dim).transpose(1, 2)
 
@@ -81,25 +83,25 @@ def MistralAttention_fast_forward(
         kv_seq_len += past_key_value[0].shape[-2]
 
     # Extend RoPE dynamically to fit in VRAM
-    self.rotary_emb.extend_rope_embedding(V, seq_len = kv_seq_len)
+    self.rotary_emb.extend_rope_embedding(V, seq_len=kv_seq_len)
 
     if position_ids is None:
         cos = self.rotary_emb.cos_cached
         sin = self.rotary_emb.sin_cached
         Q, K = fast_rope_embedding(Q, K, cos, sin)
     else:
-        cos, sin = self.rotary_emb(V, seq_len = kv_seq_len)
+        cos, sin = self.rotary_emb(V, seq_len=kv_seq_len)
         Q, K = inplace_rope_embedding(Q, K, cos, sin, position_ids)
     pass
 
     if past_key_value is not None:
-        K = torch.cat([past_key_value[0], K], dim = 2)
-        V = torch.cat([past_key_value[1], V], dim = 2)
+        K = torch.cat([past_key_value[0], K], dim=2)
+        V = torch.cat([past_key_value[1], V], dim=2)
     pass
     past_key_value = (K, V) if use_cache else None
 
     # Attention module
-    if (not HAS_FLASH_ATTENTION and attention_mask is None):
+    if not HAS_FLASH_ATTENTION and attention_mask is None:
         # Xformers memory efficient attention
         Q = Q.transpose(1, 2)
         K = K.transpose(1, 2)
@@ -110,8 +112,8 @@ def MistralAttention_fast_forward(
         has_swa = isinstance(causal_mask, xformers.attn_bias.BlockDiagonalCausalMask)
 
         # Group query attention
-        K = K  .view(bsz, kv_seq_len, n_kv_heads,        1, head_dim)
-        V = V  .view(bsz, kv_seq_len, n_kv_heads,        1, head_dim)
+        K = K.view(bsz, kv_seq_len, n_kv_heads, 1, head_dim)
+        V = V.view(bsz, kv_seq_len, n_kv_heads, 1, head_dim)
         K = K.expand(bsz, kv_seq_len, n_kv_heads, n_groups, head_dim)
         V = V.expand(bsz, kv_seq_len, n_kv_heads, n_groups, head_dim)
         if hidden_states.requires_grad:
@@ -134,7 +136,7 @@ def MistralAttention_fast_forward(
             pass
         pass
 
-        A = xformers_attention(Q, K, V, attn_bias = causal_mask)
+        A = xformers_attention(Q, K, V, attn_bias=causal_mask)
         A = A.view(bsz, q_len, n_heads, head_dim)
 
     elif HAS_FLASH_ATTENTION and attention_mask is None:
@@ -144,7 +146,7 @@ def MistralAttention_fast_forward(
         sw = getattr(self.config, "sliding_window", None)
         sw = kv_seq_len if (sw is None or sw == "null") else sw
         window = (-1, -1) if (kv_seq_len <= sw) else (sw, sw)
-        A = flash_attn_func(Q, K, V, causal = True, window_size = window)
+        A = flash_attn_func(Q, K, V, causal=True, window_size=window)
     else:
         # Grouped query attention
         # if n_groups != 1:
@@ -158,15 +160,19 @@ def MistralAttention_fast_forward(
         Q, K, V = Q.contiguous(), K.contiguous(), V.contiguous()
         # Needs (batch_size, n_heads, seq_len, head_dim)
         # is_casual and attention_mask must not be both set!
-        A = scaled_dot_product_attention(Q, K, V, attn_mask = attention_mask, is_causal = False)
+        A = scaled_dot_product_attention(
+            Q, K, V, attn_mask=attention_mask, is_causal=False
+        )
         # Go back to (batch_size, seq_len, n_heads, head_dim)
         A = A.transpose(1, 2).contiguous()
     pass
-    
-    attn_output = A.reshape(bsz, q_len, n_heads*head_dim)
+
+    attn_output = A.reshape(bsz, q_len, n_heads * head_dim)
     attn_output = self.apply_o(self, attn_output)
     attn_weights = None
     return attn_output, attn_weights, past_key_value
+
+
 pass
 
 
@@ -185,7 +191,8 @@ def MistralForCausalLM_fast_forward(
     return_dict: Optional[bool] = None,
     num_logits_to_keep: Optional[int] = 0,
     logits_to_keep: Optional[int] = 0,
-    *args, **kwargs,
+    *args,
+    **kwargs,
 ) -> Union[Tuple, CausalLMOutputWithPast]:
 
     if causal_mask is None and past_key_values is None:
@@ -196,16 +203,24 @@ def MistralForCausalLM_fast_forward(
         elif q_len <= sliding_window:
             causal_mask = xformers.attn_bias.LowerTriangularMask()
         else:
-            causal_mask = xformers.attn_bias.BlockDiagonalCausalMask\
-                .from_seqlens([q_len]*bsz)\
-                .make_local_attention(window_size = sliding_window)
+            causal_mask = xformers.attn_bias.BlockDiagonalCausalMask.from_seqlens(
+                [q_len] * bsz
+            ).make_local_attention(window_size=sliding_window)
     pass
 
-    output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
-    output_hidden_states = (
-        output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+    output_attentions = (
+        output_attentions
+        if output_attentions is not None
+        else self.config.output_attentions
     )
-    return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+    output_hidden_states = (
+        output_hidden_states
+        if output_hidden_states is not None
+        else self.config.output_hidden_states
+    )
+    return_dict = (
+        return_dict if return_dict is not None else self.config.use_return_dict
+    )
 
     # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
     self.model._has_no_labels = labels is None
@@ -215,21 +230,21 @@ def MistralForCausalLM_fast_forward(
             self,
             input_ids,
             past_key_values,
-            position_ids = position_ids,
-            attention_mask = attention_mask,
+            position_ids=position_ids,
+            attention_mask=attention_mask,
         )
     else:
         outputs = self.model(
-            input_ids = input_ids,
-            causal_mask = causal_mask,
-            attention_mask = attention_mask,
-            position_ids = position_ids,
-            past_key_values = past_key_values,
-            inputs_embeds = inputs_embeds,
-            use_cache = use_cache,
-            output_attentions = output_attentions,
-            output_hidden_states = output_hidden_states,
-            return_dict = return_dict,
+            input_ids=input_ids,
+            causal_mask=causal_mask,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
         )
     pass
 
@@ -238,10 +253,11 @@ def MistralForCausalLM_fast_forward(
     bsz, q_len, hd = hidden_states.shape
     lm_head = self.lm_head.weight
     lm_head_device = lm_head.device
-    
+
     # Move items to same device as lm_head
     hidden_states = hidden_states.to(lm_head_device)
-    if labels is not None: labels = labels.to(lm_head_device)
+    if labels is not None:
+        labels = labels.to(lm_head_device)
 
     # If we are in GRPO mode, return raw hidden states
     if os.environ.get("UNSLOTH_RETURN_HIDDEN_STATES", "0") == "1":
@@ -249,11 +265,11 @@ def MistralForCausalLM_fast_forward(
         if num_logits_to_keep != 0:
             hidden_states = hidden_states[:, -num_logits_to_keep:, :]
         return CausalLMOutputWithPast(
-            loss = None,
-            logits = hidden_states,
-            past_key_values = outputs.past_key_values,
-            hidden_states = outputs.hidden_states,
-            attentions = outputs.attentions,
+            loss=None,
+            logits=hidden_states,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
         )
     pass
 
@@ -261,33 +277,38 @@ def MistralForCausalLM_fast_forward(
         logits = torch.mv(lm_head, hidden_states.ravel().to(lm_head.dtype))
         logits = logits.unsqueeze(0).unsqueeze(0)
     elif num_logits_to_keep != 0:
-        logits = self.lm_head(hidden_states[:, -num_logits_to_keep:, :].to(lm_head.dtype))
+        logits = self.lm_head(
+            hidden_states[:, -num_logits_to_keep:, :].to(lm_head.dtype)
+        )
     else:
         RETURN_LOGITS = os.environ.get("UNSLOTH_RETURN_LOGITS", "0") == "1"
         # < 1024 Normal Unsloth uses less VRAM!
-        if bsz * q_len <= 1024: RETURN_LOGITS = True
+        if bsz * q_len <= 1024:
+            RETURN_LOGITS = True
 
         if not RETURN_LOGITS and HAS_CUT_CROSS_ENTROPY and labels is not None:
-            n_items = kwargs.get("num_items_in_batch", None) or kwargs.get("n_items", None)
+            n_items = kwargs.get("num_items_in_batch", None) or kwargs.get(
+                "n_items", None
+            )
             logit_softcapping = getattr(self.config, "final_logit_softcapping", 0)
             loss = fused_linear_cross_entropy(
-                hidden_states = hidden_states,
-                lm_weight = lm_head,
-                labels = labels,
-                num_items_in_batch = n_items,
-                logit_softcapping = logit_softcapping,
+                hidden_states=hidden_states,
+                lm_weight=lm_head,
+                labels=labels,
+                num_items_in_batch=n_items,
+                logit_softcapping=logit_softcapping,
             )
 
             if not return_dict:
                 output = (logits,) + outputs[1:]
                 return (loss,) + output if loss is not None else output
-            
+
             output = CausalLMOutputWithPast(
-                loss = loss,
-                logits = EMPTY_LOGITS,
-                past_key_values = outputs.past_key_values,
-                hidden_states = outputs.hidden_states,
-                attentions = outputs.attentions,
+                loss=loss,
+                logits=EMPTY_LOGITS,
+                past_key_values=outputs.past_key_values,
+                hidden_states=outputs.hidden_states,
+                attentions=outputs.attentions,
             )
             return output
         pass
@@ -307,9 +328,10 @@ def MistralForCausalLM_fast_forward(
         shift_labels[..., :-1] = labels[..., 1:]
         shift_labels[..., -1] = -100
         loss = fast_cross_entropy_loss(
-            logits  = shift_logits,
-            labels  = shift_labels,
-            n_items = kwargs.get("num_items_in_batch", None) or kwargs.get("n_items", None),
+            logits=shift_logits,
+            labels=shift_labels,
+            n_items=kwargs.get("num_items_in_batch", None)
+            or kwargs.get("n_items", None),
         )
     pass
 
@@ -318,12 +340,14 @@ def MistralForCausalLM_fast_forward(
         return (loss,) + output if loss is not None else output
 
     return CausalLMOutputWithPast(
-        loss = loss,
-        logits = logits,
-        past_key_values = outputs.past_key_values,
-        hidden_states = outputs.hidden_states,
-        attentions = outputs.attentions,
+        loss=loss,
+        logits=logits,
+        past_key_values=outputs.past_key_values,
+        hidden_states=outputs.hidden_states,
+        attentions=outputs.attentions,
     )
+
+
 pass
 
 
@@ -342,6 +366,8 @@ def patch_mistral_nemo_attention(function):
         "self.o_proj = nn.Linear(self.config.num_attention_heads * self.head_dim, self.config.hidden_size, bias=False)",
     )
     return function
+
+
 pass
 
 
@@ -350,66 +376,72 @@ class FastMistralModel(FastLlamaModel):
     @staticmethod
     def pre_patch():
         init_name, function = patch_linear_scaling(
-            model_name         = "mistral",
-            rope_module        = LlamaRotaryEmbedding,
-            scaled_rope_module = LlamaLinearScalingRotaryEmbedding,
-            attention_module   = MistralAttention,
+            model_name="mistral",
+            rope_module=LlamaRotaryEmbedding,
+            scaled_rope_module=LlamaLinearScalingRotaryEmbedding,
+            attention_module=MistralAttention,
         )
         # Just for Mistral Nemo models!
         if function is not None and init_name is not None:
             function = patch_mistral_nemo_attention(function)
             # if True:#init_name is not None:
             exec(function, globals())
-            MistralAttention.__init__  = eval(init_name)
+            MistralAttention.__init__ = eval(init_name)
         pass
-        MistralAttention      .forward = MistralAttention_fast_forward
-        MistralSdpaAttention  .forward = MistralAttention_fast_forward
+        MistralAttention.forward = MistralAttention_fast_forward
+        MistralSdpaAttention.forward = MistralAttention_fast_forward
         MistralFlashAttention2.forward = MistralAttention_fast_forward
-        MistralDecoderLayer   .forward = LlamaDecoderLayer_fast_forward
-        MistralModel          .forward = LlamaModel_fast_forward
-        MistralForCausalLM    .forward = MistralForCausalLM_fast_forward
-        PeftModelForCausalLM  .forward = PeftModelForCausalLM_fast_forward
+        MistralDecoderLayer.forward = LlamaDecoderLayer_fast_forward
+        MistralModel.forward = LlamaModel_fast_forward
+        MistralForCausalLM.forward = MistralForCausalLM_fast_forward
+        PeftModelForCausalLM.forward = PeftModelForCausalLM_fast_forward
         fix_prepare_inputs_for_generation(MistralForCausalLM)
-        
+
         # Solves https://github.com/unslothai/unsloth/issues/168
         # Static KV Cache was introduced in 4.38.0, causing training to be much slower.
         # Inferene can now be CUDAGraphed, but we shall retain the old rotary embeddings.
         # https://github.com/huggingface/transformers/pull/27931
         # https://github.com/huggingface/transformers/blob/v4.37.2/src/transformers/models/llama/modeling_llama.py
         import transformers.models.mistral.modeling_mistral
-        transformers.models.mistral.modeling_mistral.MistralRotaryEmbedding = LlamaRotaryEmbedding
-        return
-    pass
 
+        transformers.models.mistral.modeling_mistral.MistralRotaryEmbedding = (
+            LlamaRotaryEmbedding
+        )
+        return
+
+    pass
 
     @staticmethod
     def from_pretrained(
-        model_name        = "unsloth/mistral-7b-bnb-4bit",
-        max_seq_length    = None,
-        dtype             = None,
-        load_in_4bit      = True,
-        token             = None,
-        device_map        = "sequential",
-        rope_scaling      = None, # Mistral does not support RoPE scaling
-        fix_tokenizer     = True,
-        model_patcher     = None,
-        tokenizer_name    = None,
-        trust_remote_code = False,
+        model_name="unsloth/mistral-7b-bnb-4bit",
+        max_seq_length=None,
+        dtype=None,
+        load_in_4bit=True,
+        token=None,
+        device_map="sequential",
+        rope_scaling=None,  # Mistral does not support RoPE scaling
+        fix_tokenizer=True,
+        model_patcher=None,
+        tokenizer_name=None,
+        trust_remote_code=False,
         **kwargs,
     ):
         return FastLlamaModel.from_pretrained(
-            model_name        = model_name,
-            max_seq_length    = max_seq_length,
-            dtype             = dtype,
-            load_in_4bit      = load_in_4bit,
-            token             = token,
-            device_map        = device_map,
-            rope_scaling      = rope_scaling,
-            fix_tokenizer     = fix_tokenizer,
-            model_patcher     = FastMistralModel,
-            tokenizer_name    = tokenizer_name,
-            trust_remote_code = trust_remote_code,
+            model_name=model_name,
+            max_seq_length=max_seq_length,
+            dtype=dtype,
+            load_in_4bit=load_in_4bit,
+            token=token,
+            device_map=device_map,
+            rope_scaling=rope_scaling,
+            fix_tokenizer=fix_tokenizer,
+            model_patcher=FastMistralModel,
+            tokenizer_name=tokenizer_name,
+            trust_remote_code=trust_remote_code,
             **kwargs,
         )
+
     pass
+
+
 pass
